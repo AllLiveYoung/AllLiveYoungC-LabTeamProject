b9f505c1ffdee9e574621b3d26ad354e26e35b0b
AllLiveYoung <hansung0705@naver.com>
2021-12-05 23:49:53 +0900

    asdf

--- a.js
+++ a.js
@@ -424,3 +424,9 @@ gsap.fromTo(header, { backgroundColor: "white" }, {
 
 
 
+// d3.selectAll("span")
+//     .datum(function () { return this.datset })
+//     .style("height", "10%")
+//     .style("left", (d.i)=>(i * 80) + "px")
+//     .transition().duration(1500)
+//     .style("height", d => d.val + "%");
